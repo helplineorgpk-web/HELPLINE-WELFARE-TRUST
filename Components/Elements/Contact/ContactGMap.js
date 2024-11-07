cb1c42c47aa29001f73c9c87cb7af0efa486d59b
mudasir <smudasir256@gmail.com>
2024-11-07 19:00:52 +0500

    add data in side bar

--- Components/Elements/Contact/ContactGMap.js
+++ Components/Elements/Contact/ContactGMap.js
@@ -2,20 +2,16 @@ import React from "react";
 
 export default function ContactGMap() {
   return (
-    //google map area start
-
     <div className="contact_map_area">
       <div className="container-fluid g-0">
         <div className="contact_map">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d193595.91489303828!2d-74.1197634!3d40.6974034!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2z4Kao4Ka_4KaJIOCmh-Cmr-CmvOCmsOCnjeCmlSwg4Kau4Ka-4Kaw4KeN4KaV4Ka_4KaoIOCmr-CngeCmleCnjeCmpOCmsOCmvuCmt-CnjeCmn-CnjeCmsA!5e0!3m2!1sbn!2sbd!4v1618211532935!5m2!1sbn!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6803.149432957353!2d74.328493!3d31.4604549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919037611000001%3A0xf4b9227f93dded6d!2sHOUSE%20%23%20705%2FA-1%2C%20STREET%20NO.%2011%2C%20GOVT%20EMPLOYEE%20CO-OPERATIVE%20HOUSING%20SOCIETY%2C%20TOWNSHIP%2C%20Lahore!5e0!3m2!1sen!2sbd!4v1605272373598!5m2!1sen!2sbd"
             allowFullScreen=""
             loading="lazy"
           ></iframe>
         </div>
       </div>
     </div>
-
-    //google map area end
   );
 }
